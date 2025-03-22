2c15071240e88378b42acbfc706c12b687a62b6a
Daniel X <xuboxdan@gmail.com>
2025-03-22 15:15:44 -0500

    fix: Just implemented some CSS fixes.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,73 +1,140 @@
-import Image from 'next/image';
-import styles from '../styles/Header.module.scss';
-import { Poppins } from '@next/font/google';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { BsGearFill } from 'react-icons/bs';
+import Image from "next/image";
+import styles from "../styles/Header.module.scss";
+import { Poppins } from "@next/font/google";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { BsGearFill } from "react-icons/bs";
 
-import React from 'react';
+import React from "react";
 
-import SettingsPicker from './SettingsPicker';
-
+import SettingsPicker from "./SettingsPicker";
 
 const poppins = Poppins({
-    weight: '400',
-    subsets: ['latin']
+  weight: "400",
+  subsets: ["latin"],
 });
 
 export default function Header() {
-    const router = useRouter();
+  const router = useRouter();
 
-    // @ts-ignore
-    const [ settingsHidden, setIsSettingsHidden ] = React.useState(true);
+  // @ts-ignore
+  const [settingsHidden, setIsSettingsHidden] = React.useState(true);
 
-    const [ navHidden, setNavHidden ] = React.useState(true);
+  const [navHidden, setNavHidden] = React.useState(true);
 
-    function toggleSettingsVisibility() {
-      setIsSettingsHidden((prevSettingsHidden: boolean) => !prevSettingsHidden);
-    }
+  function toggleSettingsVisibility() {
+    setIsSettingsHidden((prevSettingsHidden: boolean) => !prevSettingsHidden);
+  }
 
-    function toggleNavHidden() {
-      setNavHidden((prevNavStatus: boolean) => !prevNavStatus);
-    }
+  function toggleNavHidden() {
+    setNavHidden((prevNavStatus: boolean) => !prevNavStatus);
+  }
 
-    return (
-      <div className={styles.holder}>
-        <SettingsPicker hidden={settingsHidden} />
-        <header
-          className={poppins.className + ' ' + styles.header}
-          role='banner'
-        >
-          <Image
-            src="/logo.svg"
-            alt="My Logo"
-            className={styles.myLogo}
-            width={150}
-            height={36}
-            priority
-          />
-          <div className={styles.headerEnd}>
-            <nav role="navigation" className={`${styles.navbar}${navHidden ? ' ' + styles.revealed : ''}`}>
-              <ul>
-                <li><Link href="/" className={router.pathname === '/' ? styles.active : ''}>Home</Link></li>
-                <li><Link href="/About" className={router.pathname === '/About' ? styles.active : ''}>About</Link></li>
-                <li><Link href="/Work" className={router.pathname === '/Work' ? styles.active : ''}>Work</Link></li>
-                <li><Link href="/blog" className={router.pathname === '/blog' ? styles.active : ''}>Blog</Link></li>
-                <li><Link href="/Contact" className={router.pathname === '/Contact' ? styles.active : ''}>Contact</Link></li>
-              </ul>
-            </nav>
-            <button onClick={toggleSettingsVisibility}>
-              <BsGearFill />
-            </button>
-            <button className={`${styles.navbarBtn}${navHidden ? '' : ' ' + styles.navbarBtnOpen}`} onClick={toggleNavHidden}>
-              <svg className={`${styles.hamburger}`} viewBox="0 0 40 40">
-                <rect fill="currentColor" x="4" y="11" width="32" height="2" rx="2" ry="1" />
-                <rect fill="currentColor" x="4" y="19" width="32" height="2" rx="2" ry="1" />
-                <rect fill="currentColor" x="4" y="27" width="32" height="2" rx="2" ry="1" />
-              </svg>
-            </button>
-          </div>
-        </header>
-      </div>
-    );
+  return (
+    <div className={styles.holder}>
+      <SettingsPicker hidden={settingsHidden} />
+      <header className={poppins.className + " " + styles.header} role="banner">
+        <Image
+          src="/logo.svg"
+          alt="My Logo"
+          className={styles.myLogo}
+          width={150}
+          height={60}
+          priority
+        />
+        <div className={styles.headerEnd}>
+          <nav
+            role="navigation"
+            className={`${styles.navbar}${
+              navHidden ? " " + styles.revealed : ""
+            }`}
+          >
+            <ul>
+              <li>
+                <Link
+                  href="/"
+                  className={router.pathname === "/" ? styles.active : ""}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/About"
+                  className={router.pathname === "/About" ? styles.active : ""}
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/Work"
+                  className={router.pathname === "/Work" ? styles.active : ""}
+                >
+                  Work
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/blog"
+                  className={router.pathname === "/blog" ? styles.active : ""}
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/Contact"
+                  className={
+                    router.pathname === "/Contact" ? styles.active : ""
+                  }
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <button onClick={toggleSettingsVisibility}>
+            <BsGearFill />
+          </button>
+          <button
+            className={`${styles.navbarBtn}${
+              navHidden ? "" : " " + styles.navbarBtnOpen
+            }`}
+            onClick={toggleNavHidden}
+          >
+            <svg className={`${styles.hamburger}`} viewBox="0 0 40 40">
+              <rect
+                fill="currentColor"
+                x="4"
+                y="11"
+                width="32"
+                height="2"
+                rx="2"
+                ry="1"
+              />
+              <rect
+                fill="currentColor"
+                x="4"
+                y="19"
+                width="32"
+                height="2"
+                rx="2"
+                ry="1"
+              />
+              <rect
+                fill="currentColor"
+                x="4"
+                y="27"
+                width="32"
+                height="2"
+                rx="2"
+                ry="1"
+              />
+            </svg>
+          </button>
+        </div>
+      </header>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 // @ts-ignore
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import styles from '../styles/Home.module.scss';
-import { FaLinkedinIn, FaGithubSquare, FaTwitter } from 'react-icons/fa';
+import styles from "../styles/Home.module.scss";
+import { FaLinkedinIn, FaGithubSquare, FaTwitter } from "react-icons/fa";
 
-import Base from '../components/Base';
-import { SettingsContext } from '../components/SettingsContext';
-
+import Base from "../components/Base";
+import { SettingsContext } from "../components/SettingsContext";
 
 export default function Home() {
   const { settings } = useContext(SettingsContext);
@@ -30,26 +29,35 @@ export default function Home() {
             <div id={styles.icons}>
               {/* 
               // @ts-ignore */}
-              <Link href="https://github.com/dXu23" className={styles.iconShadow}>
+              <Link
+                href="https://github.com/dXu23"
+                className={styles.iconShadow}
+              >
                 <FaGithubSquare />
               </Link>
               {/* 
               // @ts-ignore */}
-              <Link href="https://linkedin.com/in/dan-x" className={styles.iconShadow}>
-                <FaLinkedinIn/>
+              <Link
+                href="https://linkedin.com/in/dan-x"
+                className={styles.iconShadow}
+              >
+                <FaLinkedinIn />
               </Link>
               {/* 
               // @ts-ignore */}
-              <Link href="https://twitter.com/dydevx" className={styles.iconShadow}>
-                <FaTwitter/>
+              <Link
+                href="https://twitter.com/dydevx"
+                className={styles.iconShadow}
+              >
+                <FaTwitter />
               </Link>
             </div>
           </div>
           <p className={styles.intro}>
-            I develop and design user experiences to meet their needs and wants. 
+            I develop and design user experiences to meet their needs and wants.
           </p>
         </main>
       </Base>
     </>
-  )
+  );
 }
